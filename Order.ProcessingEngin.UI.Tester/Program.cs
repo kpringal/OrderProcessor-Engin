6b985e4accacadc9d622fb03a251a70a559331b7
pringal koyani <kpringal@outlook.com>
2020-08-30 16:47:56 +0530

    Added OrderProcessor

--- Order.ProcessingEngin.UI.Tester/Program.cs
+++ Order.ProcessingEngin.UI.Tester/Program.cs
@@ -1,15 +1,93 @@
-﻿using System;
+﻿using Order.ProcessingEngin.Common;
+using Order.ProcessingEngin.Factories;
+using Order.ProcessingEngin.Factories.Interfaces;
+using Order.ProcessingEngin.Orders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Order.ProcessingEngin.UI.Tester
+namespace Order.ProcessingEngin.UI
 {
     class Program
     {
         static void Main(string[] args)
         {
+        TOP:
+            Console.Clear();
+            var orderEnum = GetOrderneedToBeproccessed();
+
+            if (orderEnum.HasValue)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                IRuleCommandFacotry rcf = new RuleCommandFacotry();
+                OrderProcessor op = new OrderProcessor(orderEnum.Value, rcf, true);
+                op.RunCommands();
+
+                Console.WriteLine();
+                Console.WriteLine("Press C to Continue processing order.");
+                var temp = Console.ReadLine();
+                if (temp.ToLower().Equals("c"))
+                    goto TOP;
+            }
+            else
+            {
+                Console.WriteLine("Invalid orderEnum value, press R to try again");
+                var temp = Console.ReadLine();
+                if (temp.ToLower().Equals("r"))
+                    goto TOP;
+            }
+        }
+
+
+        private static OrderEnum? GetOrderneedToBeproccessed()
+        {
+            Console.WriteLine("Select order to be proccessed");
+
+            Console.WriteLine($"Press 1 for {OrderEnum.Book} order");
+            Console.WriteLine($"Press 2 for {OrderEnum.LearningToSki} order");
+            Console.WriteLine($"Press 3 for {OrderEnum.MemberShip} order");
+            Console.WriteLine($"Press 4 for {OrderEnum.PhysicalProduct} order");
+            Console.WriteLine($"Press 5 for {OrderEnum.UpgradeMemberShip} order");
+
+            string temp = Console.ReadLine();
+            int input = 0;
+            OrderEnum? orderEnum = null;
+
+            if (!int.TryParse(temp, out input))
+            {
+                Console.WriteLine("Invalid user input, press R to try again");
+                temp = Console.ReadLine();
+                if (temp.ToLower().Equals("r"))
+                    return GetOrderneedToBeproccessed();
+                else
+                    return null;
+            }
+
+            if (input <= 0 && input > 5)
+            {
+                Console.WriteLine("Invalid user input, press R to try again");
+                temp = Console.ReadLine();
+                if (temp.ToLower().Equals("r"))
+                    return GetOrderneedToBeproccessed();
+                else
+                    return null;
+            }
+
+            if (input == 1)
+                orderEnum = OrderEnum.Book;
+            else if (input == 2)
+                orderEnum = OrderEnum.LearningToSki;
+            else if (input == 3)
+                orderEnum = OrderEnum.MemberShip;
+            else if (input == 4)
+                orderEnum = OrderEnum.PhysicalProduct;
+            else if (input == 5)
+                orderEnum = OrderEnum.UpgradeMemberShip;
+
+            return orderEnum;
         }
     }
 }
---
+++ Order.ProcessingEngin/Orders/OrderProcessor.cs
@@ -0,0 +1,77 @@
+﻿using Order.ProcessingEngin.Common;
+using Order.ProcessingEngin.Factories.Interfaces;
+using Order.ProcessingEngin.RuleCommands.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Order.ProcessingEngin.Orders
+{
+    public class OrderProcessor
+    {
+        public readonly OrderEnum Order;
+        public readonly IRuleCommandFacotry RuleCommandFacotry;
+        public readonly bool IsContinueOnFail;
+        protected IEnumerable<IRuleCommand> RuleCommands { get; private set; }
+
+        public OrderProcessor(OrderEnum order, IRuleCommandFacotry ruleCommandFacotry, bool isContinueOnFail)
+        {
+            Order = order;
+            RuleCommandFacotry = ruleCommandFacotry;
+            IsContinueOnFail = isContinueOnFail;
+            Console.WriteLine($"Order processor created for {Order.ToString()}");
+        }
+
+
+        public int RunCommands()
+        {
+            var RuleCommands = GetRulesCommands().ToArray();
+            Console.WriteLine($"Below Rules have been created");
+            Console.WriteLine($"{ string.Join("\n", RuleCommands.Select(x => x.RuleCommand))}");
+            var commands = RuleCommands.Count();
+            var failcommands = 0;
+            foreach (IRuleCommand ruleCommand in RuleCommands)
+            {
+                var result = ruleCommand.Execute();
+                failcommands += result ? 0 : 1;
+                if (!result && !IsContinueOnFail)
+                {
+                    Console.WriteLine($"Stop running remaining ruls as IsContinueOnFail: {IsContinueOnFail} and {ruleCommand.RuleCommand} completed with Error");
+                    break;
+                }
+            }
+            Console.WriteLine($"{failcommands } failed out of {commands}");
+            return failcommands;
+        }
+
+        private IEnumerable<IRuleCommand> GetRulesCommands()
+        {
+            Console.WriteLine($"Creating rules for {Order.ToString()}");
+
+            switch (Order)
+            {
+                case OrderEnum.PhysicalProduct:
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.GeneratePackingSlip);
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.GenerateCommisionPayment);
+                    break;
+                case OrderEnum.Book:
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.GenerateDuplicatePackingSlip);
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.GenerateCommisionPayment);
+                    break;
+                case OrderEnum.MemberShip:
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.ActivateMembership);
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.SendMail);
+                    break;
+                case OrderEnum.UpgradeMemberShip:
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.UpgradeMembership);
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.SendMail);
+                    break;
+                case OrderEnum.LearningToSki:
+                    yield return RuleCommandFacotry.GetRuleCommands(RuleCommandEnum.AddFirsAidVideo);
+                    break;
+            }
+
+            Console.WriteLine($"Rules created for {Order.ToString()}");
+        }
+    }
+}
